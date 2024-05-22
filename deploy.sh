a360c3202f29f4316c87fa305600e9cdf1f004db
minyoung <alsdud11737@gmail.com>
2024-05-22 17:24:23 +0900

    deploy edit

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,11 @@ echo "Writing AppSpec file..."
 sed 's|LATEST_TASK_DEFINITION_ARN_PLACEHOLDER|'"$TASK_DEFINITION_ARN"'|g' appspec.yml > appspec-temp.yml
 
 echo "Triggering deployment..."
+APPSPEC_CONTENT=$(jq -Rs . < appspec-temp.yml)
 DEPLOYMENT_ID=$(aws deploy create-deployment \
   --application-name AppECS-app-cluster-was-service \
   --deployment-group-name DgpECS-app-cluster-was-service \
-  --revision "{\"revisionType\": \"AppSpecContent\", \"appSpecContent\": {\"content\": \"$(jq -Rs . < appspec-temp.yml)\"}}" \
+  --revision "{\"revisionType\": \"AppSpecContent\", \"appSpecContent\": {\"content\": $APPSPEC_CONTENT}}" \
   --query 'deploymentId' --output text)
 
 if [ -z "$DEPLOYMENT_ID" ]; then
