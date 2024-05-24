183c3b76d21dbdf3d563815249a84cf1c7179e7f
minyoung <alsdud11737@gmail.com>
2024-05-24 15:36:42 +0900

    assets navbar

--- src/main/java/com/example/mydata/controller/UserController.java
+++ src/main/java/com/example/mydata/controller/UserController.java
@@ -36,13 +36,22 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<?> loginMember(@RequestBody Map<String, String> loginData, HttpServletRequest request) {
         System.out.println("Received login data: " + loginData); // 디버깅 로그 추가
+        System.out.println("이게 로그인 데이터 컨트롤러" + loginData);
         User loginUser = service.loginUser(loginData);
+        System.out.println("그래서 로그인 된 사람이 누구냐면  컨트롤러" + loginUser);
+
         HttpSession session = request.getSession();
         if (loginUser != null) {
+            System.out.println("그래서 로그인 된 사람이 누구냐면 아아디  컨트롤러" + loginUser.getId());
+            System.out.println("그래서 로그인 된 사람이 누구냐면 비밀번호 컨트롤러" + loginUser.getPassword());
+            System.out.println("그래서 로그인 된 사람이 누구냐면 이름 컨트롤러" + loginUser.getUsername());
             session.setAttribute("username", loginUser.getUsername());
+            session.setAttribute("userid", loginUser.getId());
+
             HashMap<String, Object> response = new HashMap<>();
             response.put("success", true);
             response.put("username", loginUser.getUsername());
+            response.put("userid", loginUser.getId());
             return ResponseEntity.ok(response);
         } else {
             HashMap<String, Object> response = new HashMap<>();
@@ -51,5 +60,18 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
     }
+    @RequestMapping("/assets")
+    public ModelAndView manageAssets(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("username") != null) {
+            ModelAndView mav = new ModelAndView("assets"); // 자산 관리 페이지로 이동
+            mav.addObject("username", session.getAttribute("username"));
+            mav.addObject("userid", session.getAttribute("userid")); // userid 추가
+            return mav;
+        } else {
+            return new ModelAndView("redirect:/login"); // 로그인 페이지로 리다이렉트
+        }
+    }
+
 
 }
